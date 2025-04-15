56b2c23775fc4bd4a3f427401cab93184bbf2e6b
Vsturmo <turnovszky.moritz@gmail.com>
2025-04-15 14:16:34 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+
+const root = document.getElementById("root");
+root.innerHTML = `
+  <div style='padding: 2rem; font-family: sans-serif;'>
+    <h1>🌊 Road to Grandmaster</h1>
+    <p>This is your deployed prototype. Use the full React build for advanced interactivity.</p>
+  </div>
+`;
